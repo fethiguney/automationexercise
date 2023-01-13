8ee12b2f4ed7f9d5da21e3cbc5757f79130c53be
Fethi <fethiguneyy@gmail.com>
2023-01-13 23:17:09 +0300

    13/01/2023

---
+++ src/test/java/com/automationexercise/stepDefinitions/Hooks.java
@@ -0,0 +1,19 @@
+package com.automationexercise.stepDefinitions;
+
+
+import com.automationexercise.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+public class Hooks {
+    @After
+    public void tearDown(Scenario scenario){ final byte[]screenshot=((TakesScreenshot)
+            Driver.getDriver()).getScreenshotAs(OutputType.BYTES); if (scenario.isFailed()){
+        scenario.attach(screenshot,"image/png","screenshots");
+    }
+        Driver.closeDriver();
+    }
+}
